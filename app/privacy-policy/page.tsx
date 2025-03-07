8ca41d15d67f62386e2f360fe2ec3feeb2162aed
Kelviny de Jesus Santos <134507778+Kelviny-Jesus@users.noreply.github.com>
2025-03-07 19:29:38 -0300

    Create page.tsx
    privacy policy page

---
+++ app/privacy-policy/page.tsx
@@ -0,0 +1,146 @@
+"use client";
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="container mx-auto p-6 max-w-4xl text-white bg-[#0A0B14] min-h-screen">
+      <h2 className="text-2xl font-bold mb-4">
+        Privacy Policy
+      </h2>
+      <p className="mb-4">
+        Your privacy is important to us. It is NB1's policy to respect your privacy regarding any
+        information we may collect from you across our website,{" "}
+        <a href="https://nb1.ai/" className="text-blue-400 hover:underline">
+          NB1
+        </a>
+        , and other sites we own and operate.
+      </p>
+      <p className="mb-4">
+        We only ask for personal information when we truly need it to provide a service to you. We
+        collect it by fair and lawful means, with your knowledge and consent. We also let you know
+        why we’re collecting it and how it will be used.
+      </p>
+      <p className="mb-4">
+        We only retain collected information for as long as necessary to provide you with your
+        requested service. What data we store, we’ll protect within commercially acceptable means to
+        prevent loss and theft, as well as unauthorised access, disclosure, copying, use or
+        modification.
+      </p>
+      <p className="mb-4">
+        We don’t share any personally identifying information publicly or with third-parties, except
+        when required to by law.
+      </p>
+      <p className="mb-4">
+        Our website may link to external sites that are not operated by us. Please be aware that we
+        have no control over the content and practices of these sites, and cannot accept
+        responsibility or liability for their respective{" "}
+        <a
+          href="https://privacypolicies.in/"
+          rel="noopener noreferrer"
+          target="_blank"
+          className="text-blue-400 hover:underline"
+        >
+          privacy policies
+        </a>
+        .
+      </p>
+      <p className="mb-4">
+        You are free to refuse our request for your personal information, with the understanding
+        that we may be unable to provide you with some of your desired services.
+      </p>
+      <p className="mb-4">
+        Your continued use of our website will be regarded as acceptance of our practices around
+        privacy and personal information. If you have any questions about how we handle user data
+        and personal information, feel free to contact us.
+      </p>
+      <h2 className="text-xl font-bold mt-6 mb-4">
+        Cookie Policy for NB1
+      </h2>
+      <p className="mb-4">
+        This is the Cookie Policy for NB1, accessible from URL{" "}
+        <a href="https://nb1.ai/" className="text-blue-400 hover:underline">
+          https://nb1.ai/
+        </a>
+        .
+      </p>
+      <h3 className="text-lg font-semibold mt-4 mb-2">
+        What Are Cookies
+      </h3>
+      <p className="mb-4">
+        As is common practice with almost all professional websites this site uses cookies, which
+        are tiny files that are downloaded to your computer, to improve your experience. This page
+        describes what information they gather, how we use it and why we sometimes need to store
+        these cookies. We will also share how you can prevent these cookies from being stored
+        however this may downgrade or break certain elements of the sites functionality.
+      </p>
+      <h3 className="text-lg font-semibold mt-4 mb-2">
+        How We Use Cookies
+      </h3>
+      <p className="mb-4">
+        We use cookies for a variety of reasons detailed below. Unfortunately in most cases there
+        are no industry standard options for disabling cookies without completely disabling the
+        functionality and features they add to this site. It is recommended that you leave on all
+        cookies if you are not sure whether you need them or not in case they are used to provide a
+        service that you use.
+      </p>
+      <h3 className="text-lg font-semibold mt-4 mb-2">
+        Disabling Cookies
+      </h3>
+      <p className="mb-4">
+        You can prevent the setting of cookies by adjusting the settings on your browser (see your
+        browser Help for how to do this). Be aware that disabling cookies will affect the
+        functionality of this and many other websites that you visit. Disabling cookies will usually
+        result in also disabling certain functionality and features of this site. Therefore it is
+        recommended that you do not disable cookies.
+      </p>
+      <h3 className="text-lg font-semibold mt-4 mb-2">
+        The Cookies We Set
+      </h3>
+      <p className="mb-4">
+        <br />
+      </p>
+      <h3 className="text-lg font-semibold mt-4 mb-2">
+        Third Party Cookies
+      </h3>
+      <p className="mb-4">
+        In some special cases we also use cookies provided by trusted third parties. The following
+        section details which third party cookies you might encounter through this site.
+      </p>
+      <p className="mb-4">
+        <br />
+      </p>
+      <h3 className="text-lg font-semibold mt-4 mb-2">
+        User's Responsibilities
+      </h3>
+      <p className="mb-4">
+        The user undertakes the responsibility to make appropriate use of the contents and
+        information offered on the site with enunciative, but not imitative, behaviour:
+      </p>
+      <ul className="list-disc pl-5 mb-4">
+        <li>
+          A) Not to engage in activities that are illegal or contrary to good faith and public
+          order;
+        </li>
+        <li>
+          B) Not to spread propaganda or content of a racist, xenophobic or gambling nature, any
+          type of illegal pornography, terrorist claims or against human rights;
+        </li>
+        <li>
+          C) Do not cause damage to physical systems (hardware) and unattainable (software) of NB1,
+          its suppliers or third parties, to introduce or disseminate computer viruses or any other
+          hardware or software systems that are capable of causing damage previously mentioned.
+        </li>
+      </ul>
+      <h3 className="text-lg font-semibold mt-4 mb-2">
+        More Information
+      </h3>
+      <p className="mb-4">
+        Hopefully that has clarified things for you and as was previously mentioned if there is
+        something that you aren't sure whether you need or not it's usually safer to leave cookies
+        enabled in case it does interact with one of the features you use on our site.
+      </p>
+      <p className="mb-4">
+        This policy is effective as of 14 February 2025 19:59.
+      </p>
+    </div>
+  );
+}
