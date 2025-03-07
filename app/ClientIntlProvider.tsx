d3e0c2a3913ddeb490be617f0d4eb83f2ab84144
Kelviny de Jesus Santos <134507778+Kelviny-Jesus@users.noreply.github.com>
2025-03-07 19:30:54 -0300

    Update ClientIntlProvider.tsx
    update of versions

--- app/ClientIntlProvider.tsx
+++ app/ClientIntlProvider.tsx
@@ -1,24 +1,28 @@
-"use client"
+"use client";
 
-import { type ReactNode, useState } from "react"
-import { IntlProvider } from "react-intl"
-import en from "@/locales/en.json"
-import es from "@/locales/es.json"
-import pt from "@/locales/pt.json"
+import { type ReactNode, useState } from "react";
+import { IntlProvider } from "react-intl";
+import en from "@/locales/en.json";
+import es from "@/locales/es.json";
+import pt from "@/locales/pt.json";
 
-const messages = {
+// Definir o tipo das chaves do objeto messages
+type Locale = "en" | "es" | "pt";
+
+// Tipar o objeto messages como um Record com chaves do tipo Locale e valores do tipo Record<string, string>
+const messages: Record<Locale, Record<string, string>> = {
   en,
   es,
   pt,
-}
+};
 
 export default function ClientIntlProvider({ children }: { children: ReactNode }) {
-  const [locale, setLocale] = useState("en")
+  const [locale, setLocale] = useState<Locale>("en");
 
   return (
     <IntlProvider messages={messages[locale]} locale={locale} defaultLocale="en">
       {children}
     </IntlProvider>
-  )
+  );
 }
 
